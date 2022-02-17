84584030073d54b8e5ba9fadb5c364c54d37b7d6
Jens Hardt <jenshardt@gmx.net>
2022-02-17 22:45:59 +0100

    added knights

---
+++ src/main/java/de/jenshardt/entity/Knight.java
@@ -0,0 +1,16 @@
+package de.jenshardt.entity;
+
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+
+public class Knight extends PanacheMongoEntity {
+
+    public String name;
+    public String swordName;
+
+    public Knight() { }
+
+    public Knight(String name, String swordName) {
+        this.name = name;
+        this.swordName = swordName;
+    }
+}
